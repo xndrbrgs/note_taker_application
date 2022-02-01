2b95d7b8f6feb0b6415e3dcea871394786fbb373
Alex Borges <aborgescolon@gmail.com>
2022-02-01 10:28:09 -0500

    added server js file

---
+++ server.js
@@ -0,0 +1,9 @@
+// Global constants for all requires 
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Global constants for express 
+const app = express();
+const PORT = process.env.PORT || 3001; 
+
